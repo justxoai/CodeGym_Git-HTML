8098bda9b0cfb69cb3e55efc609c9bb41e3256d7
justxoai <tuti12032004@gmail.com>
2025-12-29 15:21:09 +0700

    Update Message Exercise

---
+++ ShowMessage/showmessage.js
@@ -0,0 +1,15 @@
+function alertMessage(){
+    alert("Hello Word by using Alert");
+}
+
+function htmnlMessage(){
+    document.getElementById("result").innerHTML = "Hello Word by using innerHTML";
+}
+
+function documentMessage(){
+    document.write("Hello Word by using Write");
+}
+
+function consoleMessage(){
+    console.log("Hello Word b using Console")
+}
