7886dcbe15a5c8c05e06fe9f7bc0be905c6e2beb
justxoai <tuti12032004@gmail.com>
2026-01-19 23:30:35 +0700

    Update Date Class Exercise

---
+++ DateClass/exercise.js
@@ -0,0 +1,6 @@
+let todayDate = new MyDate(19, 1, 2026);
+
+document.write(todayDate.getDay());
+document.write(todayDate.getMonth());
+document.write(todayDate.getYear());
+document.write(todayDate.toString());
